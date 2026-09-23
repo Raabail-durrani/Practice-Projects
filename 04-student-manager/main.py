ce4023699992b8125823b9783e4793d8c3a243e9
Raabail Durrani <raabaildurrani.1m@email.com>
2026-09-23 11:47:29 +0500

    feat: add student management system CLI

---
+++ 04-student-manager/main.py
@@ -0,0 +1,102 @@
+students = []
+
+ 
+
+def display_menu():
+    print("==== STUDENT MANAGER ====")
+    print("01. Add students")
+    print("02. View all students")
+    print("03. Add/update grades")
+    print("04. View student Report Card")
+    print("05. Delete student")
+    print("06. Exit")
+
+def add_Student():
+    student_name = input(" Enter student name: ").strip()
+    if student_name == "":
+        print("NO RECORD")
+        return
+
+    roll_number = int(input(" STUDENT ROLL NO. : "))
+    if not roll_number.isdigit():
+        print(" VALUE ERROR: ENTER NUMBER ONLY")
+        return
+    if any(student["roll_number"] == roll_number for student in students):
+        print(" ERROR: Student with this roll number already exists")
+        return
+    roll_number = int(roll_number)
+    new_student={ "student_name":student_name,
+                 "roll_number": roll_number,
+                  "grades": {} 
+                  }
+    students.append(new_student)
+    print(f"student {student_name} added succesfully to record!! :> <3")
+
+
+
+    
+def view_student():
+    if len(students) == 0:
+        print("NOTHING RECORDED YET!!")
+
+    else:
+        for student in students:
+            print(f"{student['student_name']} | {student['roll_number']} |{student['grades']} ")
+
+            
+
+
+def add_update_grades():
+    pass
+
+
+
+
+def view_report():
+    pass
+
+
+
+
+def delete_student():
+    pass
+
+
+
+def main():
+    while True:
+        display_menu()
+
+        choice = input (" Select option from 1 - 6: " )
+
+        if choice == "1":
+            add_Student()
+
+        elif choice == "2":
+            print("Viewing all students feature coming soon!!!")
+            view_student()
+
+        elif choice == "3":
+            print("Adding or updating student's record feature coming soon")
+            add_update_grades()
+
+        elif choice == "4":
+            print("Viewing student report card feature coming ssoon!!!")
+            view_report()
+
+        elif choice == "5":
+            print("deleting a student record coming soon!!!")
+            delete_student()
+
+        elif choice == "6":
+            print("EXIT!!!!")
+            break
+
+        else:
+            print("INVALID OPTION!")
+
+
+
+
+if __name__ == "__main__":
+    main()
